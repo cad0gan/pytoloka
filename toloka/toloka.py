e8b3d0f45a11b15c9f28c20136e313ea556d8fe9
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-08 12:49:54 +0400

    Toloka class

--- toloka/toloka.py
+++ toloka/toloka.py
@@ -1,2 +1,10 @@
-class Toloka:
-    pass
+import aiohttp
+from yandex import Yandex
+
+
+class Toloka(Yandex):
+    async def request_tasks(self):
+        async with aiohttp.ClientSession(headers=self._headers, cookie_jar=self._cookies) as session:
+            response = await session.get('https://toloka.yandex.ru/api/i-v2/task-suite-pool-groups')
+            json = await response.json()
+            return json
