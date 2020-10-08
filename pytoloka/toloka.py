9c97fe7fa42620a606544c7ae1ef3e53ca399bd2
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-08 16:00:12 +0400

    Rename method

--- pytoloka/toloka.py
+++ pytoloka/toloka.py
@@ -3,7 +3,7 @@ from pytoloka.yandex import Yandex
 
 
 class Toloka(Yandex):
-    async def request_tasks(self):
+    async def get_tasks(self):
         async with aiohttp.ClientSession(headers=self._headers, cookie_jar=self._cookies) as session:
             response = await session.get('https://toloka.yandex.ru/api/i-v2/task-suite-pool-groups')
             json = await response.json()
