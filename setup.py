ddc7df9d0853fd27df3877cc8ef05fe6b21822be
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-19 01:48:16 +0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='pytoloka',
+    version='0.0.1',
+    author='Philip Minakov',
+    author_email='sir.cadogan@protonmail.com',
+    packages=setuptools.find_packages(),
+)
